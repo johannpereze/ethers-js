8e0b735d76e85f5eef55444fb6ff76379e65cc73
johannpereze <sebas.jsp@gmail.com>
2024-12-28 12:13:52 -0500

    feat(app): added theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
-import "./App.css";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { ModeToggle } from "./components/mode-toggle";
+import { ThemeProvider } from "./components/theme-provider";
 
 function App() {
-  return <>hello world</>;
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div>
+        <ModeToggle />
+      </div>
+      <div className="flex justify-center w-full p-4">
+        <div className="min-w-[16rem] max-w-xl w-full">
+          <Card>
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
