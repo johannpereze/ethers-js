801d94f0db7a85d930939b0c5d3e96db6c85586f
johannpereze <sebas.jsp@gmail.com>
2024-12-28 21:27:51 -0500

    feat(app): installed ethersjs and fetched balance

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,29 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { formatEther, JsonRpcProvider } from "ethers";
+import { useEffect, useState } from "react";
 import { ModeToggle } from "./components/mode-toggle";
 import { ThemeProvider } from "./components/theme-provider";
 
+const INFURA_URL = import.meta.env.VITE_INFURA_URL;
+const API_KEY = import.meta.env.VITE_API_KEY;
+const account = "0x73BCEb1Cd57C711feaC4224D062b0F6ff338501e";
+
 function App() {
+  const [balance, setBalance] = useState<string | null>(null);
+  const provider = new JsonRpcProvider(`${INFURA_URL}/${API_KEY}`);
+
+  useEffect(() => {
+    (async () => {
+      const newBalance = await provider.getBalance(account);
+      setBalance(formatEther(newBalance));
+    })();
+  }, []);
+
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div>
@@ -19,15 +34,15 @@ function App() {
         <div className="min-w-[16rem] max-w-xl w-full">
           <Card>
             <CardHeader>
-              <CardTitle>Card Title</CardTitle>
-              <CardDescription>Card Description</CardDescription>
+              <CardTitle>JP Wallet</CardTitle>
+              <CardDescription>ETH Balance</CardDescription>
             </CardHeader>
             <CardContent>
-              <p>Card Content</p>
+              <p>{balance} ETH</p>
             </CardContent>
-            <CardFooter>
+            {/* <CardFooter>
               <p>Card Footer</p>
-            </CardFooter>
+            </CardFooter> */}
           </Card>
         </div>
       </div>
